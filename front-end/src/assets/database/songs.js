991a160348754aebf1d2be4401fe1a50ab207db3
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-02-16 22:25:19 -0300

    Banco de dados em json e logo

---
+++ front-end/src/assets/database/songs.js
@@ -0,0 +1,266 @@
+// export const songsArray = [
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24",
+//     name: "Última Saudade - Ao Vivo",
+//     duration: "2:30",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 1,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d",
+//     name: "Xonei",
+//     duration: "2:34",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 2,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02656f8291d741ad247ef330fe",
+//     name: "Paredões",
+//     duration: "2:55",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 3,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24",
+//     name: "Amigo Da Minha Saudade - Ao Vivo",
+//     duration: "2:30",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 4,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24",
+//     name: "Seja Ex - Ao Vivo",
+//     duration: "3:12",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 5,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920",
+//     name: "De Trás Pra Frente - Ao Vivo",
+//     duration: "2:43",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 6,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920",
+//     name: "Aquela Pessoa - Ao Vivo",
+//     duration: "2:46",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 7,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e0299c540c0f78793fcea84a920",
+//     name: "Meu Amor - Ao Vivo",
+//     duration: "2:54",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 8,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31",
+//     name: "Como É Que A Gente Fica - Ao Vivo",
+//     duration: "2:43",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 9,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24",
+//     name: "Romântico - Ao Vivo",
+//     duration: "3:03",
+//     artist: "Henrique & Juliano",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/ultima-saudade.mp3",
+//     id: 10,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02d8bfb4567b1b5e5bc4da915b",
+//     name: "Oh Garota Eu Quero Você Só Pra Mim",
+//     duration: "2:46",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 11,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02f412f43fad4c3ffab163287c",
+//     name: "Barbie",
+//     duration: "3:12",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 12,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02905f574e927e59ada3fc2c97",
+//     name: "Do Job",
+//     duration: "3:13",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 13,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02dc16c0b3939b95e09076ec5e",
+//     name: "A Danada Me Ligando",
+//     duration: "4:43",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 14,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e0299ca4be43858f41a3bad9728",
+//     name: "2025",
+//     duration: "2:52",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 15,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02a8e9382ce68ad5d8758bd998",
+//     name: "E Aí Como Que Tá ?",
+//     duration: "2:14",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 16,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e022d1affc39c1329a4e3138424",
+//     name: "THE BOX MEDLEY FUNK 2",
+//     duration: "4:26",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 17,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02320f4a08913b939a8637c301",
+//     name: "Boy Besta",
+//     duration: "7:04",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 18,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02700a3fcafbd8498c78f16cb0",
+//     name: "Vida de Artista",
+//     duration: "3:16",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 19,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e0212d096992d1a94a34672ecd2",
+//     name: "Mó Fita",
+//     duration: "4:16",
+//     artist: "MC Tuto",
+//     audio: "https://jornada-fullstack.s3.us-east-2.amazonaws.com/barbie.mp3",
+//     id: 20,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d",
+//     name: "Cantada Boba - Ao Vivo",
+//     duration: "3:03",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 21,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d",
+//     name: "Xonei",
+//     duration: "2:34",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 22,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d",
+//     name: "Haverá Sinais - Ao Vivo",
+//     duration: "3:11",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 23,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02afcda189d3e44b4abefddecb",
+//     name: "Logo Eu",
+//     duration: "2:35",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 24,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e028aca7c95468481cf5411060e",
+//     name: "Amo Noite E Dia - Live In Sao Paulo / 2010",
+//     duration: "3:03",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 25,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02c0e0704dbc02116c26cfea45",
+//     name: "Marca Evidente",
+//     duration: "3:09",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 26,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02d50fbb2113e9d82d0677abce",
+//     name: "Paredes - Ao Vivo",
+//     duration: "3:04",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 27,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02047ad702243f8d6ff1318fe0",
+//     name: "Anti Amor - Ao Vivo",
+//     duration: "2:45",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 28,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d",
+//     name: "Tem Amor, Tem Volta - Ao Vivo",
+//     duration: "2:22",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 29,
+//   },
+//   {
+//     image: "https://i.scdn.co/image/ab67616d00001e02d50fbb2113e9d82d0677abce",
+//     name: "Pra Sempre Com Você - Ao Vivo",
+//     duration: "3:27",
+//     artist: "Jorge & Mateus",
+//     audio:
+//       "https://jornada-fullstack.s3.us-east-2.amazonaws.com/cantada-boba.mp3",
+//     id: 30,
+//   },
+// ];
+
+import { songsArray } from "../../../api/api.js";
+
+export { songsArray }
