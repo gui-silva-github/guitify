d98724f27b22ff62d29b0dbec8773a001bfb1201
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-02-16 22:24:37 -0300

    API de conexão com o back-end

---
+++ front-end/api/api.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+// const { NODE_ENV } = process.env
+const URL = "https://guitify.onrender.com/api"
+
+const responseArtists = await axios.get(`${URL}/artists`)
+const responseSongs = await axios.get(`${URL}/songs`)
+
+export const artistArray = responseArtists.data
+export const songsArray = responseSongs.data
+
